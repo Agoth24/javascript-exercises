105b600536a5149e76c7511899b98bd66e476cff
Agoth24 <agoth.a3@gmail.com>
2025-06-17 23:28:13 -0600

    Completed all of these practice problems
    Took me way too long though. It was good for learning how
    to look things up and read documentation efficiently.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumericals = 'abcdefghijklmnopqrstuvqxyz0123456789'
+  const cleanedString = string
+  .toLowerCase()
+  .split('')
+  .filter((character) => alphanumericals.includes(character))
+  .join('');
 
+  const reversedString = cleanedString.split('').reverse().join('');
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+//   const currentDate = new Date();
+//   if (person.yearOfDeath === undefined) {
+//     person.yearOfDeath = currentDate.getFullYear();
+//   }
+//   return person.yearOfDeath - person.yearOfBirth;
 
+// MORE EFFICIENT WAY
+const currentYear = new Date().getFullYear();
+const deathYear = person.yearOfDeath ?? currentYear;
+return deathYear - person.yearOfBirth;
 };
 
+const findTheOldest = function (array) {
+  let oldest = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (getAge(array[i]) > getAge(oldest)) {
+      oldest = array[i];
+    }
+  }
+  return oldest;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
